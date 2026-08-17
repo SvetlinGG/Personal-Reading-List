ed4431f2ce8f0f4ea8aa1645142b708267d78f4e
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-17 21:54:20 +0200

    work with res json datra

--- src/script/api.js
+++ src/script/api.js
@@ -1,3 +1,14 @@
 
 const API_URL = "/src/data/sample-books.json";
-const API_KEY = ""
+const API_KEY = "";
+
+async function fetchJSON(params){
+    const res = await fetch(`${API_URL}`);
+    //console.log(res);
+    const data = await res.json();
+    console.log(data);
+    
+    return data;
+
+}
+

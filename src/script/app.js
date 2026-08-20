d1fc66dcfc6e383b264a7bb2d1109ccaa724fa5f
Svetlin Garabedyan <igscosmetics@gmail.com>
2026-08-20 22:08:47 +0200

    set loading function

--- src/script/app.js
+++ src/script/app.js
@@ -1,1 +1,20 @@
-import { searchBooks, getBooksById } from './api.js'
+import { searchBooks, getBooksById } from './api.js';
+
+const el = {
+    form: document.getElementById('searchForm'),
+    q: document.getElementById('q'),
+    resultsGrid: document.getElementById('resultsGrid'),
+    cardTpl: document.getElementById('cardTpl'),
+    
+}
+
+const state = {
+    q: '',
+    page: 1,
+    total: 0
+}
+
+function setLoading(){
+    el.resultsGrid.innerHTML = '<p>Loading...</p>';
+
+}
